a2b91f2d3beeac43d491f3efac17d38821f2f03c
Hugo Enrique Atayde <hugo.e.atayde@gmail.com>
2023-02-11 15:47:31 -0600

    part10 commit, added some comments, adjusted some line spacing

--- SEAssignment3/src/main.java
+++ SEAssignment3/src/main.java
@@ -15,7 +15,13 @@ public class main {
 		
 		System.out.println("Sum of Array: " + ArraySum(t));
 		System.out.println("Product of Array: " + ArrayProd(t));
+<<<<<<< Upstream, based on origin/master
 		System.out.println("Reverse Array: " + ReverseArray(t).toString());
+=======
+		
+		//Checkedout Commit #3, created new branch part10
+		//Reformated some lines
+>>>>>>> e950e6d part10 commit, added some comments, adjusted some line spacing
 
 	}
 	
@@ -25,19 +31,17 @@ public class main {
 		for(int i = 0; i < t.size(); i++) {
 			result += t.get(i);
 		}
-		
 		return result;
 	}
-	
 	public static int ArrayProd(ArrayList<Integer> t) {
 		int result = 1;
 		
 		for(int i = 0; i < t.size(); i++) {
 			result *= t.get(i);
 		}
-		
 		return result;
 	}
+<<<<<<< Upstream, based on origin/master
 	
 	public static ArrayList<Integer> ReverseArray(ArrayList<Integer> t) {
 		ArrayList<Integer> result = new ArrayList<Integer>();
@@ -50,4 +54,6 @@ public class main {
 	}
 	
 
+=======
+>>>>>>> e950e6d part10 commit, added some comments, adjusted some line spacing
 }
